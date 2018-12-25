7ba1f1ae6391a7df4e358cdfd3be2a994af75809
peps0791 <piyush.papreja07@gmail.com>
2018-12-25 13:14:31 -0700

    lane marking assistance frontend added

--- public/js/uploadnew.js
+++ public/js/uploadnew.js
@@ -1,7 +1,76 @@
 window.isMouseDown = false;
+window.isEditMode = false;
+window.isAssistanceEnabled = false;
+
+
+$("#highlightBtn").click(function(){
+
+    console.log("highlights button clicked...");
+
+    let start = $("#startPt").val();
+    let end = $("#endPt").val();
+
+    //TODO: do initial checks to validate coordinates
+
+    console.log(start);
+    console.log(end);
+
+    let x1 = parseInt(start.split("-")[0]);
+    let y1 = parseInt(start.split("-")[1]);
+
+    let x2 = parseInt(end.split("-")[0]);
+    let y2 = parseInt(end.split("-")[1]);
+
+    if(x1===x2){
+        for(let i=y1;i<y2;i++){
+            $("#" + x1 + "-" + i).addClass("highlighted")
+        }
+
+    }else if(y1===y2){
+        for(let i=x1;i<x2;i++){
+            $("#" + i + "-" + y1).addClass("highlighted")
+        }
+    }
+
+});
+
+$('#enableAsstCheckBox').click(function() {
+    console.log("Checkbox state (method 1) = " + $('#enableAsstCheckBox').prop('checked'));
+    console.log("Checkbox state (method 2) = " + $('#enableAsstCheckBox').is(':checked'));
+
+    if($('#enableAsstCheckBox').prop('checked')){
+        $("#assistant").attr("hidden", false);
+        window.isEditMode = false;
+        window.isAssistanceEnabled = true;
+    }else{
+        $("#assistant").attr("hidden", true);
+        window.isEditMode = true;
+        window.isAssistanceEnabled = false;
+    }
+});
+
+
+// get selection
+$('.editMode input[type=radio]').on('change', function() {
+    console.log(this.value);
+    //
+    let el = $("#enableAsstDiv");
+    if(this.value === "EditMode"){
+        el.attr("hidden", false);
+        window.isEditMode = true;
+        window.isAssistanceEnabled = false;
+        $('#enableAsstCheckBox').prop('checked', false);
+
+    }else if(this.value === "ViewMode"){
+        el.attr("hidden", true);
+        $("#assistant").attr("hidden", true);
+        window.isEditMode = false;
+        window.isAssistanceEnabled = false;
+    }
+});
 
 $(document).mousedown(function(event) {
-    if(event.target.nodeName === "TD"){
+    if(window.isEditMode &&  event.which === 1 &&  event.target.nodeName === "TD"){
 
         window.isMouseDown = true;
         $(event.target).toggleClass("highlighted");
@@ -10,14 +79,14 @@ $(document).mousedown(function(event) {
         $("#seatCoord").val(event.target.id)
     }
 }).mouseover(function (event) {
-    if(event.target.nodeName === "TD"){
+    if(window.isEditMode &&  event.which === 1 &&  event.target.nodeName === "TD"){
         if (window.isMouseDown) {
             $(event.target).toggleClass("highlighted");
         }
     }
 
 }).mouseup(function (event) {
-    if(event.target.nodeName === "TD"){
+    if(window.isEditMode &&  event.which === 1 && event.target.nodeName === "TD"){
         window.isMouseDown = false;
     }
 });
@@ -26,7 +95,23 @@ $(document).mousedown(function(event) {
 $('#clearAllBtn').click(function () {
 
     //clear all highlighted buttons
-    console.log('clear all button clicked...')
+    console.log('clear all button clicked...');
+
+    if(window.confirm("Are you sure you want to remove all the markings?\nThis action cannot be undone!")){
+        let nodes = [];
+        let el_arr = $(".highlighted");
+
+        for (let item of el_arr) {
+            item.classList.remove('highlighted')
+        }
+        $.post("/save-map", {"nodes": nodes, "mapname":localStorage.getItem("mapname")}, function (data) {
+            console.log("result from server:" + JSON.stringify(data));
+            alert("Markings removed successfully!");
+
+            clearAllTextBoxes();
+        });
+    }
+
 });
 
 $('#saveMapBtn').click(function () {
@@ -120,3 +205,11 @@ $('#searchBtn').click(function () {
         $("#" + itemid).addClass("searchresult");
     }
 });
+
+
+function clearAllTextBoxes(){
+    $('#seatCoord').val("");
+    $("#seatNo").val("");
+    $("#empName").val("");
+    $("#empPhone").val("");
+}
